3958fd2497ccb8d5b7ca4b713e69e91485d10fcf
Tansimrinky <tansimrinkybindu@gmail.com>
2023-09-14 20:48:54 +0600

    header added

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,7 +1,8 @@
 const Header = () => {
     return (
-        <div>
+        <div className="flex justify-between content-center flex-wrap mx-4 p-10 border-b-2">
             <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+            <img src="src\assets\images\profile.png" alt="" />
         </div>
     );
 };
